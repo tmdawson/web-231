a9313fdf649957e0bdf012e3c4426fe8b5491179
Tisa Dawson <tmdawson@my365.bellevue.edu>
2019-12-01 17:25:56 -0600

    Adding dawson-assignment-1.4.js

---
+++ week-1/dawson-assignment-1.4.js
@@ -0,0 +1,16 @@
+/*============================================; 
+Title: Assignment 1.4; 
+Author: Professor Krasso ; 
+Date: 25 November 2019; 
+Modified By: Tisa Dawson; 
+Description: Primitive Types Assignment;
+===========================================*/ 
+
+var firstName = "Tisa";
+var lastName = "Dawson";
+var age = 57;
+var street = "111 Way Street";
+var city = "Melbourne FL";
+var zipCode = 90012;
+
+console.log(firstName + " " + " " + lastName + ", " + age + ", " + street + ", " + city + ", " + zipCode);
