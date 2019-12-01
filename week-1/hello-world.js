3a75078b5373ccbc33894701d428f902586b49a0
Tisa Dawson <tmdawson@my365.bellevue.edu>
2019-12-01 17:02:39 -0600

    Adding hello-world.js

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,9 @@
+/*============================================; 
+Title: Assignment 1.3; 
+Author: Professor Krasso ; 
+Date: 25 November 2019; 
+Modified By: <Your name>; 
+Description: This assignment is to create a new file with a Hello World message;
+===========================================*/ 
+
 console.log("Hello World, my name is Tisa Dawson");
